a4b9cb7148384918d3b7591e168448c6fe7dcf23
narumincho <narumincho.starfy@gmail.com>
2020-07-01 16:17:57 +0900

    update package, format code, fix style

--- hosting_root/serviceworker.js
+++ hosting_root/serviceworker.js
@@ -1,28 +1,25 @@
 self.addEventListener("push", (e) => {
-    console.log("プッシュ通知をサーバーから受け取った", e);
-    const data = e.data.json();
-    console.table(data);
+  console.log("プッシュ通知をサーバーから受け取った", e);
+  const data = e.data.json();
+  console.table(data);
 
-    e.waitUntil(
-        self.registration.showNotification(
-            data.title,
-            {
-                body: data.body,
-                icon: data.icon,
-                tag: data.tag,
-                actions: [
-                    { action: "actionA", title: "アクションボタンA" },
-                    { action: "actionB", title: "アクションボタンB" }
-                ],
-                data: {
-                    link_to: data.link
-                }
-            }
-        )
-    );
+  e.waitUntil(
+    self.registration.showNotification(data.title, {
+      body: data.body,
+      icon: data.icon,
+      tag: data.tag,
+      actions: [
+        { action: "actionA", title: "アクションボタンA" },
+        { action: "actionB", title: "アクションボタンB" },
+      ],
+      data: {
+        link_to: data.link,
+      },
+    })
+  );
 });
 
 self.addEventListener("notificationclick", (e) => {
-    e.notification.close();
-    e.waitUntil(clients.openWindow(e.notification.data.link_to));
+  e.notification.close();
+  e.waitUntil(clients.openWindow(e.notification.data.link_to));
 });
